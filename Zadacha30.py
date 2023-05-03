8bb1106567f291f870897062e92bf681bf6a6c03
AlexIv <ivlieva.medphyslab.com>
2023-05-03 20:46:15 +0300

    Z30 Code has been corrected and works

--- Zadacha30.py
+++ Zadacha30.py
@@ -2,3 +2,15 @@
 # Её первый элемент, разность и количество элементов нужно ввести с клавиатуры. 
 # Формула для получения n-го члена прогрессии: an = a1 + (n-1) * d.
 # Каждое число вводится с новой строки.
+
+def arithmetic_progression_as_a_list():
+    our_progression = [int(input("Введите первый член прогрессии: "))]
+    diff_of_progression = int(input("Введите разность прогрессии: "))
+    n = int(input("Введите количество членов прогрессии: "))
+    for i in range(1, n):
+        n_i = our_progression[0] + i * diff_of_progression
+        our_progression.append(n_i)
+    return our_progression
+
+
+print(arithmetic_progression_as_a_list())
