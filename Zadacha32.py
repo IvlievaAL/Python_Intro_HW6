7660cd4b3f5cfc29982b59f6b6d430c0ca881979
AlexIv <ivlieva.medphyslab.com>
2023-05-03 20:53:35 +0300

    Z32 Code draft has been added and works but needs corrections in input and output

--- Zadacha32.py
+++ Zadacha32.py
@@ -2,3 +2,21 @@
 # Определить индексы элементов массива (списка), 
 # значения которых принадлежат заданному диапазону 
 # (т.е. не меньше заданного минимума и не больше заданного максимума)
+
+def indexes_of_values_in_range(our_list):
+    our_range_min = int(input("Введите минимальное значение диапазона: "))
+    our_range_max = int(input("Введите максимальное значение диапазона: "))
+    if our_range_min > our_range_max:
+        raise ValueError("Минимум не может превышать максимум!")
+    indexes = []
+    for i in range(0, len(our_list)):
+        if our_range_min <= our_list[i] <= our_range_max: # Синтаксис пайтона позволяет так написать.
+            indexes.append(our_list[i])
+    if len(indexes) == 0:
+        print("Значения всех элементов массива не попадают в указанный диапазон.")
+    else:
+        print(indexes)
+
+
+our_list = [5, 24, 5, 7, 9, 25, 908, 342, 500]
+indexes_of_values_in_range(our_list) 
