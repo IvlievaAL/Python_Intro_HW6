10bfe480ab4b04cb634c5921cf3a1e6327fd62ae
AlexIv <ivlieva.medphyslab.com>
2023-05-03 21:00:09 +0300

    Z32 Input now is from console, and indexes are in output. Code is completed

--- Zadacha32.py
+++ Zadacha32.py
@@ -11,12 +11,12 @@ def indexes_of_values_in_range(our_list):
     indexes = []
     for i in range(0, len(our_list)):
         if our_range_min <= our_list[i] <= our_range_max: # Синтаксис пайтона позволяет так написать.
-            indexes.append(our_list[i])
+            indexes.append(i)
     if len(indexes) == 0:
         print("Значения всех элементов массива не попадают в указанный диапазон.")
     else:
         print(indexes)
 
 
-our_list = [5, 24, 5, 7, 9, 25, 908, 342, 500]
+our_list = list(map(int, input("Введите числа через пробел: ").split()))
 indexes_of_values_in_range(our_list) 
